b8777acc34c7e016e116d6b4f75b405970410d51
Aris Ripandi <riipandi@gmail.com>
2024-11-02 22:43:02 +0700

    chore: exclude wrangler directory fro repository

--- apps/web/.wrangler/tmp/dev-7DXzDe/chunks/routes/index.mjs
+++
@@ -1,7 +0,0 @@
-import { e as eventHandler } from '../nitro/nitro.mjs'
-
-const index = eventHandler((event) => {
-  return { message: 'API Endpoint', path: event.path }
-})
-
-export { index as default }
